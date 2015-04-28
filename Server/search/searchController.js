5257ac449f7aa8f8819faebce32683572c549b92
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-04-27 19:27:19 -0700

    Parsed data returned from Bing search to determine whether or not a question is valid.
    The server now can comb through data and compare results to an array of 'valid sites.' If the ratio of valid:total is above a certain number, returns true with top 5 results

--- Server/search/searchController.js
+++ Server/search/searchController.js
@@ -1,16 +1,36 @@
 var Bing = require('./bingKeys.js')
 var _ = require('underscore')
+var validSites = require('./validSites.js')
 
 module.exports = {
 
   searchWeb: function(req, res, next){
     var query = req.body.query;
-    Bing.web(query, function(error, res, body){
-      console.log('answer!!!!!!!!!!!!!!!!!!', body.d.results);
+    var results;
+    var isValid = false;
+    Bing.web(query, function(error, resp, body){
+      var validCount = 0;
+      results = body.d.results
+      _.each(results, function(result){
+        _.each(validSites, function(site){
+          if(result.Url.indexOf(site) > -1){
+            validCount++
+          }
+        })
+      })
+      console.log('COUNT THIS BIZNESS', validCount/30)
+
+      if(validCount/30 > 0.3){
+        isValid = true;
+        results = results.splice(0,5);
+        res.send({isValid: isValid, results: results})
+      } else {
+        res.send({isValid: isValid})
+        
+      }
     },
     {
-      top: 50  // Number of results (max 50) 
+      top: 30
     });
-    next();
     }
   }
---
+++ Server/search/validSites.js
@@ -0,0 +1,16 @@
+var sites = [
+'google',
+'wikipedia',
+'stackoverflow',
+'books.google',
+'nationalgeographic',
+'noaa',
+'quora',
+'stackexchange',
+'.edu',
+'livestrong',
+'wisegeek',
+'.org'
+]
+
+module.exports= sites;
