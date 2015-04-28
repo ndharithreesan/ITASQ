70f1138fa1def21cce509794979a776f4aa08608
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-04-27 19:52:34 -0700

    Client accurately shows Yes/No based on server response

--- Client/app/search/search.js
+++ Client/app/search/search.js
@@ -3,14 +3,19 @@ angular.module('itasq.search', [])
 .controller('SearchController', ['$scope', 'Search',  
   function($scope, Search) {
 
-    $scope.answer = '';
     $scope.results = [];
 
     $scope.ask = function(){
       Search.askQuestion($scope.question)
         .then(function(res){
           // TODO: show answers 
-          console.log(res)
+          var results = res.data;
+          console.log('ANSWER', res)
+          if(results.isValid === false){
+            $scope.answer = 'YES'
+          } else {
+            $scope.answer = 'NO';
+          }
         })
     }
 
