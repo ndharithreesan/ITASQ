c95909bd375bfd1704d1a72e3df5fd729d0ff6e0
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-04-27 18:16:24 -0700

    Successfully retrieved data from Bing search. Included underscore library in order to do work on the results

--- Server/search/searchController.js
+++ Server/search/searchController.js
@@ -1,8 +1,16 @@
 var Bing = require('./bingKeys.js')
+var _ = require('underscore')
 
 module.exports = {
 
   searchWeb: function(req, res, next){
-    console.log('working?', req.body)
+    var query = req.body.query;
+    Bing.web(query, function(error, res, body){
+      console.log('answer!!!!!!!!!!!!!!!!!!', body.d.results);
+    },
+    {
+      top: 50  // Number of results (max 50) 
+    });
+    next();
+    }
   }
-}
