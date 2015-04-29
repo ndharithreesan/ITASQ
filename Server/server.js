a5bd9c7a14409be998cdd3b6ae6dfdd323fe8598
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-04-28 21:39:37 -0700

    Add error handling on mongoose connection for debugging

--- Server/server.js
+++ Server/server.js
@@ -13,6 +13,12 @@ var uri = 'mongodb://heroku_app36349004:vfirgt9eko7docia6es2e6304l@ds031812.mong
 mongoose.connect(uri);
 console.log('connected to mongoose')
 
+var db = mongoose.connection;
+
+db.on('error', function(err){
+  console.error(err)
+})
+
 app.use(session({ secret: 'donttellanyone' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
